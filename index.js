3f0e4aea1c06cceda075c7aeb36076eb15ac796a
Jason Martin <jasnmar@gmail.com>
2026-02-22 16:51:03 -0500

    Separate controls and debug data

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//Setup controls for the demo
+import("./controls.js").then((controls) => {
+  controls.controlsStartup()
+})
+
 
 //Minimal import of debug scripts / controls based on local storage setting
 const DEBUG = localStorage.getItem("DEBUG") === "true"
